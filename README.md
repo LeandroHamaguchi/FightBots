c124b882a0cfa74bb54e4df6a6cb5e1e8985fe84
LeandroHamaguchi <brasil.leandrohv@gmail.com>
2023-05-31 21:15:27 -0300

    bug fixes e organizacao de pastas e código

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Pygame
+# Fight Bots
+### Por Leandro Hamaguchi, João Eduardo e Vinicius Miranda
+
+Esse é o **Fight Bots**, jogo desenvolvido por alunos do curso de engenharia do Insper em São Paulo. 
+
+Fight Bots é um jogo de luta 2D. Você é o FoxBot e seu objetivo é derrotar o Bot inimigo e impedir sua invasão!
+
+Para rodar o jogo basta seguir os seguintes passos:
+1. Clone esse repositório em um editor de código (recomendamos o VS Code);
+2. Verifique se possui todas as bibliotecas necessárias instaladas para utilizar nesse jogo (pygame);
+3. Com as bibliotecas instaladas, agora é só rodar.
+4. Use a tecla **"W"** para mover para cima, **"S"** para mover para baixo e a tecla **"Espaço"** para atirar.
+5. Derrote o oponente no menor tempo possível!
+
+O vídeo abaixo explica mais como o jogo funciona:
+https://youtu.be/GMg5e9HYKsQ
--- main.py
+++ main.py
@@ -3,33 +3,30 @@ pygame.init()
 pygame.mixer.init()
 pygame.font.init()
 
-
-
-#configurações para a tela inicial + música de fundo
-white = (255,255,255)
-black = (0,0,0)
+#Configurações para a tela do jogo e de jogabilidade
+white = (255, 255, 255)
+black = (0, 0, 0)
+WIDTH = 800
+HEIGHT = 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Fight Bots')
 clock = pygame.time.Clock()
-font = pygame.font.SysFont(None,235)
-texto = font.render('FIGHT BOTS',True,white)
-soundtrack = pygame.mixer.music.load('soundtrack.mp3')
-pygame.mixer.music.set_volume(0.3)
-pygame.mixer.music.play()
+FPS = 25
+PLAYER_WIDTH = 200
+PLAYER_HEIGHT = 200
+ENEMY_WIDTH = 200
+ENEMY_HEIGHT = 200
 
-
-# Defina a área selecionável
-area_selecionavel_start = pygame.Rect(360, 470, 270, 100)  # Retângulo (x, y, largura, altura)
-area_selecionavel_instrucao = pygame.Rect(360, 630, 270, 100)  # Retângulo (x, y, largura, altura)
-
-# Áreas selecionáveis com hover
+#Area selecionável
+area_selecionavel_start = pygame.Rect(360, 470, 270, 100)
+area_selecionavel_instrucao = pygame.Rect(360, 630, 270, 100)
 area_selecionavel_start_hover = pygame.Rect(360, 470, 270, 100)
 area_selecionavel_instrucao_hover = pygame.Rect(360, 630, 270, 100)
 
-
-def game_intro(screen):
-
+#Tela de inicio
+def game_intro(screen, state):
     introduction = True
     while introduction:
-        
         pygame.display.flip()
         screen.fill(black)
         screen.blit(assets['start_screen'], (0, 0))
@@ -45,7 +42,7 @@ def game_intro(screen):
                 if area_selecionavel_start.collidepoint(click):
                     screen.blit(assets['screen_start_select'], (0, 0))
                 elif area_selecionavel_instrucao.collidepoint(click):
-                    screen.blit(assets['instructions_screen'], (0, 0))
+                    screen.blit(assets['instructions'], (0, 0))
                 else:
                     screen.blit(assets['start_screen'], (0, 0))
 
@@ -56,56 +53,54 @@ def game_intro(screen):
                         state = PLAYING
                         introduction = False
                     elif area_selecionavel_instrucao.collidepoint(click):
-                        state = PLAYING
+                        state = INSTRUCTIONS
                         introduction = False
-
 
     return state
 
+#Tela de instruções
+def game_instructions(screen, state):
+    instructions = True
+    while instructions:
+        pygame.display.flip()
+        screen.fill(black)
+        screen.blit(assets['instructions'], (0, 0))
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = DONE
+                instructions = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    state = BEGIN
+                    instructions = False
 
+    return state
 
-
-
-
-WIDTH = 800
-HEIGHT = 800
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Fight in Time')
-
-PLAYER_WIDTH = 200
-PLAYER_HEIGHT = 200
-ENEMY_WIDTH = 200
-ENEMY_HEIGHT = 200
+#Imagens, sons e fontes
 assets = {}
-
-assets['background'] = pygame.image.load('background.png').convert()
+assets['background'] = pygame.image.load('assets/imgs/background.png').convert()
 assets['background'] = pygame.transform.scale(assets['background'], (WIDTH, HEIGHT))
-assets['start_screen'] = pygame.image.load('Tela_Intro.png').convert()
+assets['start_screen'] = pygame.image.load('assets/imgs/Tela_Intro.png').convert()
 assets['start_screen'] = pygame.transform.scale(assets['start_screen'], (WIDTH, HEIGHT))
-assets['instructions_screen'] = pygame.image.load('instructions_scren.png').convert()
-assets['instructions_screen'] = pygame.transform.scale(assets['instructions_screen'], (WIDTH, HEIGHT))
-assets['screen_start_select'] = pygame.image.load('screen_start_select.png').convert()
+assets['instructions'] = pygame.image.load('assets/imgs/tela_instrucoes.jpg').convert()
+assets['instructions'] = pygame.transform.scale(assets['instructions'], (WIDTH, HEIGHT))
+assets['screen_start_select'] = pygame.image.load('assets/imgs/screen_start_select.png').convert()
 assets['screen_start_select'] = pygame.transform.scale(assets['screen_start_select'], (WIDTH, HEIGHT))
-assets['player_img'] = pygame.image.load('player.png').convert_alpha()
+assets['player_img'] = pygame.image.load('assets/imgs/player.png').convert_alpha()
 assets['player_img'] = pygame.transform.scale(assets['player_img'], (PLAYER_WIDTH, PLAYER_HEIGHT))
-assets['enemy_img'] = pygame.image.load('enemy.png').convert_alpha()
+assets['enemy_img'] = pygame.image.load('assets/imgs/enemy.png').convert_alpha()
 assets['enemy_img'] = pygame.transform.scale(assets['enemy_img'], (PLAYER_WIDTH, PLAYER_HEIGHT))
-assets['bullet_img'] = pygame.image.load('bullet_img.png').convert_alpha()
+assets['bullet_img'] = pygame.image.load('assets/imgs/bullet_img.png').convert_alpha()
 assets['bullet_img'] = pygame.transform.scale(assets['bullet_img'], (60, 60))
-assets['enemy_bullet_img'] = pygame.image.load('enemy_bullet.png').convert_alpha()
+assets['enemy_bullet_img'] = pygame.image.load('assets/imgs/enemy_bullet.png').convert_alpha()
 assets['enemy_bullet_img'] = pygame.transform.scale(assets['enemy_bullet_img'], (80, 80))
-assets['game_over'] = pygame.image.load('gameover.jpeg').convert_alpha()
+assets['game_over'] = pygame.image.load('assets/imgs/gameover.png').convert_alpha()
 assets['game_over'] = pygame.transform.scale(assets['game_over'], (WIDTH, HEIGHT))
-#assets['explosion_img'] = pygame.image.load('explosion.png').convert_alpha()
-#assets['explosion_img'] = pygame.transform.scale(assets['explosion_img'], (100, 100))
-assets['font'] = pygame.font.Font('valorax-font/Valorax-lg25V.otf', 28)
+assets['font'] = pygame.font.Font('assets/fonts/valorax-font/Valorax-lg25V.otf', 28)
+assets['shoot_sound'] = pygame.mixer.Sound('assets/sounds/shoot.wav')
 
-#assets['explosion_sound'] = pygame.mixer.Sound('assets/snd/explosion.wav')
-assets['shoot_sound'] = pygame.mixer.Sound('shoot.wav')
-#assets['enemy_shoot_sound'] = pygame.mixer.Sound('assets/snd/enemy_shoot.wav')
-#assets['music'] = pygame.mixer.music.load('assets/snd/music.mp3')
-
+#Jogador principal
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups, assets):
         pygame.sprite.Sprite.__init__(self)
@@ -146,6 +141,7 @@ class Player(pygame.sprite.Sprite):
             self.groups['all_sprites'].add(new_bullet)
             self.groups['all_player_shots'].add(new_bullet)
 
+#Inimigo
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, groups, assets):
         pygame.sprite.Sprite.__init__(self)
@@ -186,6 +182,7 @@ class Enemy(pygame.sprite.Sprite):
             self.groups['all_sprites'].add(new_bullet)
             self.groups['all_enemy_shots'].add(new_bullet)
 
+#Bala do jogador
 class PlayerBullet(pygame.sprite.Sprite):
     def __init__(self, assets, centery, centerx):
         pygame.sprite.Sprite.__init__(self)
@@ -193,7 +190,7 @@ class PlayerBullet(pygame.sprite.Sprite):
         self.image = assets['bullet_img']
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-        self.rect.centerx = centerx
+        self.rect.centerx = centerx - 200
         self.rect.centery = centery
         self.speedx = 12
         self.assets = assets
@@ -212,16 +209,8 @@ class PlayerBullet(pygame.sprite.Sprite):
         if pygame.sprite.collide_mask(self, enemy):
             self.kill()
             enemy.health -= 10
-            #assets['explosion_sound'].play()
-            #explosion = Explosion(enemy.rect.center, assets)
-            #all_sprites.add(explosion)
-            #state = EXPLODING
-            #explosion_group.add(explosion)
-            #enemy = Enemy(groups, assets)
-            #all_sprites.add(enemy)
-            #enemy_group.add(enemy)
-            #score += 100
 
+#Bala do inimigo
 class EnemyBullet(pygame.sprite.Sprite):
     def __init__(self, assets, centery, centerx):
         pygame.sprite.Sprite.__init__(self)
@@ -229,7 +218,7 @@ class EnemyBullet(pygame.sprite.Sprite):
         self.image = assets['enemy_bullet_img']
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-        self.rect.centerx = centerx
+        self.rect.centerx = centerx + 200
         self.rect.centery = centery
         self.shot_speed = 12
         self.speedx = self.shot_speed
@@ -250,21 +239,8 @@ class EnemyBullet(pygame.sprite.Sprite):
         if pygame.sprite.collide_mask(self, player):
             self.kill()
             player.health -= 10
-            #assets['explosion_sound'].play()
-            #explosion = Explosion(enemy.rect.center, assets)
-            #all_sprites.add(explosion)
-            #state = EXPLODING
-            #explosion_group.add(explosion)
-            #enemy = Enemy(groups, assets)
-            #all_sprites.add(enemy)
-            #enemy_group.add(enemy)
-            #score += 100
 
-#pygame.mixer.music.set_volume(0.4)
-#pygame.mixer.music.play(loops=-1)
-clock = pygame.time.Clock()
-FPS = 25
-
+#Handling sprites
 all_sprites = pygame.sprite.Group()
 all_enemy_shots = pygame.sprite.Group()
 all_player_shots = pygame.sprite.Group()
@@ -272,7 +248,6 @@ groups = {}
 groups['all_sprites'] = all_sprites
 groups['all_enemy_shots'] = all_enemy_shots
 groups['all_player_shots'] = all_player_shots
-
 player = Player(groups, assets)
 player_shot = PlayerBullet(assets, player.rect.top, player.rect.centerx)
 enemy = Enemy(groups, assets)
@@ -280,51 +255,64 @@ enemy_shot = EnemyBullet(assets, enemy.rect.top, enemy.rect.centerx)
 all_sprites.add(player)
 all_sprites.add(enemy)
 
+#Ajustes da música
+soundtrack = pygame.mixer.music.load('assets/sounds/soundtrack.mp3')
+pygame.mixer.music.set_volume(0.01)
+pygame.mixer.music.play(loops=-1)
 
+#Ajustes iniciais do loop principal do jogo
+timer = 0
+nome = ''
 DONE = 0
 PLAYING = 1
 GAMEOVER = 2
 WIN = 3
 BEGIN = 4
 SKIP = 5
-timer = 0
 NAME_TO_START=6
+INSTRUCTIONS = 7
+
+#Loop principal do jogo
 state = NAME_TO_START
-nome=''
 while state != DONE:
     screen.fill((0, 0, 0))
+
+    #Tela para digitar nome
     if state == NAME_TO_START:
         text= assets['font'].render( 'Digite seu nome: ' , True, (255, 0, 0))
         text_rect = text.get_rect()
         text_rect.midtop = (WIDTH / 2, HEIGHT / 4)
         screen.blit(text, text_rect)
+
         for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         state = DONE
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_BACKSPACE: 
                             nome-=event.unicode
-                        
                         if event.key == pygame.K_RETURN:
                             state = BEGIN
                         else:
                             nome+=event.unicode
 
-        text_surface = assets['font'].render( nome , True, (255, 0, 0))
+        text_surface = assets['font'].render(nome, True, (255, 0, 0))
         text_rect = text_surface.get_rect()
         text_rect.midtop = (WIDTH / 2, HEIGHT / 2)
         screen.blit(text_surface, text_rect)
-        
-
         pygame.display.update()
- 
-    if state == BEGIN:
-        state = game_intro(screen)
     
+    #Verificando tela de início
+    if state == BEGIN:
+        state = game_intro(screen, state)
+
+    #Verificando tela de instruções
+    if state == INSTRUCTIONS:
+        state = game_instructions(screen, state)
+    
+    #Tela de jogo principal
     if state == PLAYING:
         while state == PLAYING:
             clock.tick(FPS)
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     state = DONE
@@ -362,27 +350,27 @@ while state != DONE:
             timer += 1
             score_font_surface = assets['font'].render("{:2d}".format(timer), True, (255, 255, 0))
             text_rect = score_font_surface.get_rect()
-            text_rect.midtop = (WIDTH / 2,  10)
+            text_rect.midtop = (WIDTH / 2,  100)
             screen.blit(score_font_surface, text_rect)
 
             lives_font_surface = assets['font'].render("{:01d}".format(player.health), True, (255, 255, 0))
             text_rect = lives_font_surface.get_rect()
-            text_rect.bottomleft = (10, HEIGHT - 10)
+            text_rect.bottomleft = (80, HEIGHT - 100)
             screen.blit(lives_font_surface, text_rect)
 
             lives_font_surface = assets['font'].render("{:01d}".format(enemy.health), True, (255, 255, 0))
             text_rect = lives_font_surface.get_rect()
-            text_rect.bottomleft = (WIDTH - 80, HEIGHT - 10)
+            text_rect.bottomleft = (WIDTH - 120, HEIGHT - 100)
             screen.blit(lives_font_surface, text_rect)
-            
-            pygame.display.update()
 
+            #Verificando game over
             if state == GAMEOVER:
                 while state == GAMEOVER:
                     screen.fill((0, 0, 0))
                     screen.blit(assets['game_over'], (0, 0))
                     pygame.display.update()
                     pygame.time.wait(2000)
+
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             state = DONE
@@ -399,10 +387,9 @@ while state != DONE:
                                     player.rect.centery = HEIGHT / 2
                                     enemy.rect.centerx = WIDTH - 100
                                     enemy.rect.centery = HEIGHT / 2
-
-
+            
+            #Verificando vitória
             if state == WIN:
-   
                 with open('Tela de pontuacao.txt', 'a') as tela_tempos:
                         if timer >= 600:
                             tempo = '{0} eliminou o inimigo em {1} minutos'.format(nome,str(timer//60))
@@ -410,17 +397,16 @@ while state != DONE:
                             tempo = '{0} eliminou o inimigo em {1} segundos'.format(nome,str(timer//10))
                         tela_tempos.write(tempo)
 
-
                 text_surface = assets['font'].render('YOU WIN', True, (0, 255, 0))
                 text_rect = text_surface.get_rect()
                 text_rect.midtop = (WIDTH / 2, HEIGHT / 4)
                 screen.blit(text_surface, text_rect)
                 
                 pygame.display.update()
-                pygame.time.wait(2000)
+                pygame.time.wait(5000)
 
+                state = NAME_TO_START
 
-            
-                
+            pygame.display.update()
 
-        pygame.quit()
+pygame.quit()
